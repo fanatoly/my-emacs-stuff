f319990ad6d6f8b501c2671f01fdbb15b37578da
Anatoly Fayngelerin <fanatoly@gmail.com>
2012-06-10 20:50:02 -0400

    a small victory against ANSI escape characters in compiler output

--- compile-utils.el
+++ compile-utils.el
@@ -10,7 +10,7 @@
 (defun compilitation-to-buffer-end ()
   "Takes the compilation buffer to the end"
   (interactive)
-  (if (and (boundp 'compilation-last-buffer) (not (eq compilation-last-buffer nil)))
+  (if (and (boundp 'compilation-last-buffer) (not (eq compilation-last-buffer nil)) (bufferp 'compilation-last-buffer))
        (save-excursion 
          (set-buffer compilation-last-buffer)
          (select-window (get-buffer-window compilation-last-buffer))
@@ -19,7 +19,15 @@
    (print "No compile window available"))
    )  
 
+
+(global-set-key [f3] 'compile)
 (global-set-key [C-f3] 'bring-up-compilation)
 
 ;; making sure that compilation shows me the end
 (add-hook 'compilation-mode-hook 'compilitation-to-buffer-end)
+
+;; make sure ansi sequences are filtered out.
+(defun fanatoly-apply-ansi-color-current-buffer ()
+ (ansi-color-apply-on-region (point-min) (point-max) ) )
+
+(add-hook 'compilation-filter-hook 'fanatoly-apply-ansi-color-current-buffer)
