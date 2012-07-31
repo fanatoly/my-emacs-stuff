ff04aeda660416aca36498b56e98fe3ddde56d90
Anatoly Fayngelerin <anatoly@yieldmo.com>
2012-07-31 11:34:15 -0400

    added log keybinding

--- magit-keybindings.el
+++ magit-keybindings.el
@@ -3,3 +3,5 @@
 (global-set-key [(control ?c)(?g)(?s)] 'magit-status)
 (global-set-key [(control ?c)(?g)(?u)] 'magit-pull)
 (global-set-key [(control ?c)(?g)(?p)] 'magit-push)
+(global-set-key [(control ?c)(?g)(?l)] 'magit-log)
+(global-set-key [(control ?c)(?g)(?c)] 'magit-commit-buffer-name)
