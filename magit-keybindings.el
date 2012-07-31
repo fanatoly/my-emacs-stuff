49fda88fbe5c5594e481da457cc77b08c4f86395
Anatoly Fayngelerin <anatoly@yieldmo.com>
2012-07-31 11:23:54 -0400

    added a push binding

--- magit-keybindings.el
+++ magit-keybindings.el
@@ -1,4 +1,5 @@
 ;; Key binding for the magit plugin
 
 (global-set-key [(control ?c)(?g)(?s)] 'magit-status)
-(global-set-key [(control ?c)(?g)(?p)] 'magit-pull)
+(global-set-key [(control ?c)(?g)(?u)] 'magit-pull)
+(global-set-key [(control ?c)(?g)(?p)] 'magit-push)
