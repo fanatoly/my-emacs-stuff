483ccbe0648ac02a7214e5d91055ecd294d628e6
Anatoly Fayngelerin <anatoly@yieldmo.com>
2013-01-18 14:43:06 -0500

    added some java editing utils. only spaces for me. Thank you very much

--- ffip-conf.el
+++ ffip-conf.el
@@ -1,5 +1,5 @@
 (require 'find-file-in-project)
 (add-to-list 'ffip-patterns "*.java")
-(setq ffip-limit (* 4096 16))
+(setq ffip-limit (* 4096 64))
 (global-set-key (kbd "C-c p f") 'find-file-in-project)
 
--- jvm-utils.el
+++ jvm-utils.el
@@ -8,3 +8,15 @@
 (add-to-list 'compilation-error-regexp-alist-alist
        '(maven "\\[ERROR\\] \\(.+?\\):\\[\\([0-9]+\\),\\([0-9]+\\)\\].*"
            1 2 3))
+
+
+(add-hook 'java-mode-hook (lambda ()
+			    (setq c-basic-offset 4
+				  tab-width 4
+				  indent-tabs-mode nil)))
+
+(add-hook 'java-mode-hook
+	  (lambda ()
+	    "Treat Java 1.5 @-style annotations as comments."
+	    (setq c-comment-start-regexp "(@|/(/|[*][*]?))")
+	    (modify-syntax-entry ?@ "< b" java-mode-syntax-table)))
